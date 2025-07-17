293ed5cb5ac05e104378caed83181f1c3f0bcdb1
FadedAF00 <jokejokerking@gmail.com>
2025-07-18 00:10:53 +0100

    feat: add ESLint configuration and update .gitignore; remove unused import in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Image from "next/image";
 import { useState, useEffect } from "react";
 import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from "react-icons/fa";
 import Link from "next/link";
